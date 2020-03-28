1a8ab27a01fe33b8a14b5ecb4e2359963776e2b9
vagrant <vagrant@localhost.localdomain>
2020-03-28 12:38:05 +0000

    [Update]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,16 +10,20 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    if book.save
+     flash[:notice] = "Book was successfully destroyed."
+     redirect_to book_path(book)
+    end
   end
-
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.save
+     flash[:notice] = "Book was successfully destroyed."
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
 get "top" => "homes#top"
 resources :books
+root to: 'home#index'
 end
